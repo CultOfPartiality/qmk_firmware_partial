48b19721be38d46a8a82b2daedb734335a4cb7e3
Partial <jason@wridgway.net>
2021-12-20 23:25:42 +1100

    Added modtap layer back, so games will work

--- keyboards/sofle/keymaps/CultOfPartiality/keymap.c
+++ keyboards/sofle/keymaps/CultOfPartiality/keymap.c
@@ -24,6 +24,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_NORMAL] = LAYOUT( \
+  KC_GESC,  KC_1,  KC_2,    KC_3,    KC_4,     KC_5,                                   KC_6,   KC_7,     KC_8,    KC_9,   KC_0,    KC_BSPC, \
+  KC_TAB,   KC_Q,  KC_W,    KC_E,    KC_R,     KC_T,                                   KC_Y,   KC_U,     KC_I,    KC_O,   KC_P,    KC_DEL,  \
+  KC_TAB,   KC_A,  KC_S,    KC_D,    KC_F,     KC_G,                                   KC_H,   KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT, \
+  KC_LSFT,  KC_Z,  KC_X,    KC_C,    KC_V,     KC_B,   KC_MUTE,          XXXXXXX,      KC_N,   KC_M,     KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, \
+                   KC_LGUI, KC_LALT, KC_LCTRL, KC_SPC, TD(TD_LOWER),     TD(TD_RAISE), KC_ENT, KC_RCTRL, KC_MINS, KC_EQL \
+),
+/*
+ * NORMAL with homerow mods
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | Bspc |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Del  |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
+ * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
+ * `-----------------------------------------/       /     \      \-----------------------------------------'
+ *            | LGUI | LAlt | LCTR |Space | /Lower  /       \RAISE \  |ENTER | RCTR |  -   |  =   |
+ *            |      |      |      |      |/       /         \      \ |      |      |      |      |
+ *            `----------------------------------'           '------''---------------------------'
+ */
+
+[_MODTAP] = LAYOUT( \
   KC_GESC,  KC_1,          KC_2,         KC_3,         KC_4,         KC_5,                                   KC_6,   KC_7,         KC_8,         KC_9,         KC_0,            KC_BSPC, \
   KC_TAB,   KC_Q,          KC_W,         KC_E,         KC_R,         KC_T,                                   KC_Y,   KC_U,         KC_I,         KC_O,         KC_P,            KC_DEL,  \
   KC_TAB,   LGUI_T(KC_A),  LALT_T(KC_S), LCTL_T(KC_D), LSFT_T(KC_F), KC_G,                                   KC_H,   LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN), KC_QUOT, \
@@ -155,7 +178,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return false;
 		case KC_MODTAP:
             if (record->event.pressed) {
-                // set_single_persistent_default_layer(_MODTAP);
+                set_single_persistent_default_layer(_MODTAP);
             }
             return false;
         /* case KC_LOWER:
