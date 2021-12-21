861d765fe8fc01aa86698907ac0c92444873a188
CultOfPartiality <jason@wridgway.net>
2021-12-21 20:21:26 +1100

    First successful compilation!
    Dropped the encoders to line up
    
    Can add in later if required, will need to add keys into keymap

--- keyboards/pteron36/keymaps/CultOfPartiality/defines.h
+++ keyboards/pteron36/keymaps/CultOfPartiality/defines.h
@@ -10,3 +10,7 @@
 #define U_CPY C(KC_INS)
 #define U_CUT S(KC_DEL)
 #define U_UND KC_UNDO
+
+#define U_NP KC_NO // key is not present
+#define U_NA KC_NO // present but not available for use
+#define U_NU KC_NO // available but not used
