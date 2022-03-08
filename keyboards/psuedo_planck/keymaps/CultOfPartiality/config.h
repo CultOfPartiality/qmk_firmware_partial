73bf7d75cec0f743b03c807479bea9428242e593
CultOfPartiality <jason@wridgway.net>
2022-03-08 21:46:06 +1100

    Adjusting layout and adding autoshift

--- keyboards/psuedo_planck/keymaps/CultOfPartiality/config.h
+++ keyboards/psuedo_planck/keymaps/CultOfPartiality/config.h
@@ -47,3 +47,16 @@
 
 // Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
+
+
+
+
+#define GRAVE_ESC_CTRL_OVERRIDE
+
+// Auto Shift
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_NO_SETUP
+
+#undef  TAPPING_TERM // Remove the default vaule set in the sofle keymap
+#define TAPPING_TERM 170
