fea59d31a0c8459ca4cd1c861f308db9ff594b3c
Partial <jason@wridgway.net>
2022-03-30 23:31:21 +1100

    Cleanup and prep for putting in a print screen

--- keyboards/psuedo_planck/keymaps/CultOfPartiality/keymap.c
+++ keyboards/psuedo_planck/keymaps/CultOfPartiality/keymap.c
@@ -67,8 +67,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,      KC_MINS, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_EQL,  KC_LBRC, KC_RBRC, _______, _______, _______,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_MINS, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_PIPE,
+    _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_EQL,  KC_LBRC, KC_RBRC, _______, _______, _______,
     _______, _______, _______, XXXXXXX, _______, _______,    _______, _______, _______, _______, _______, _______
 ),
 
