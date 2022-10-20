cc1e044d17ee2aae4ef9dad5e41121e088fdfe94
Partial <jason@wridgway.net>
2022-10-21 01:13:03 +1100

    WHY MOUSE MODE NOT WORK?????

--- keyboards/jasonsbullshit/rev1/rules.mk
+++ keyboards/jasonsbullshit/rev1/rules.mk
@@ -8,6 +8,7 @@ BOOTLOADER = qmk-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes         # Console for debug
 COMMAND_ENABLE = no        # Commands for debug and configuration
