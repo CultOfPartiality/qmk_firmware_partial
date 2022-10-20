50d1ff5fb5f80e6cdd78828e4b4b40e877497763
Partial <jason@wridgway.net>
2022-10-20 23:24:31 +1100

    Fixed naming, started moving stuff to userspace

---
+++ users/cultofpartiality/config.h
@@ -0,0 +1,13 @@
+/*    Mouse Keys Configuration    */
+#define MK_KINETIC_SPEED
+#define MOUSEKEY_INTERVAL 16            //ms between cursor movements
+#define MOUSEKEY_INITIAL_SPEED 105
+#define MOUSEKEY_MOVE_DELTA 9          //Speed step size
+#define MOUSEKEY_ACCELERATED_SPEED 1600 //?
+#define MOUSEKEY_BASE_SPEED 1800        //Max speed at which acceleration stops
+
+
+/*    Auto Shift  */
+#define NO_AUTO_SHIFT_ALPHA
+#define AUTO_SHIFT_TIMEOUT TAPPING_TERM
+#define AUTO_SHIFT_NO_SETUP
---
+++ users/cultofpartiality/cultofpartiality.c
@@ -0,0 +1,3 @@
+
+#include "cultofpartiality.h"
+
