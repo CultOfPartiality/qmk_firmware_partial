fed51890c035aadbaaf0a8b16a313ea1688b4ea0
Partial <jason@wridgway.net>
2022-10-21 01:27:37 +1100

    Quick layout fix

--- users/cultofpartiality/rules.mk
+++ users/cultofpartiality/rules.mk
@@ -2,6 +2,6 @@ SRC += cultofpartiality.c
 
 
 AUTO_SHIFT_ENABLE = yes
-MOUSEKEY_ENABLE = yes
-CAPS_WORD_ENABLE = yes # Single word capslock? Should be useful
-COMMAND_ENABLE = no # Can't use this at the same time as Caps Word
+MOUSEKEY_ENABLE   = yes
+CAPS_WORD_ENABLE  = yes # Single word capslock? Should be useful
+COMMAND_ENABLE    = no  # Can't use this at the same time as Caps Word
