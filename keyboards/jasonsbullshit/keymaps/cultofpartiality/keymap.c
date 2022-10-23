3bb46e298507f2b0516981e44fbef589cd8f31e5
Partial <jason@wridgway.net>
2022-10-23 17:56:08 +1100

    Need to work out what to do with the outer rows...

--- keyboards/jasonsbullshit/keymaps/cultofpartiality/keymap.c
+++ keyboards/jasonsbullshit/keymaps/cultofpartiality/keymap.c
@@ -57,11 +57,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `---------'-----------------------------------------------------------------------------------'---------'
  */
 [_QWERTY] = LAYOUT_ortho_5x14(
-  KC_GESC, /**/ KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   /**/ KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, /**/ KC_BSPC,
-  KC_2,    /**/ KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   /**/ KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  /**/ KC_7,
-  KC_3,    /**/ KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   /**/ KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, /**/ KC_8,
-  KC_4,    /**/ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   /**/ KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, /**/ KC_9,
-  KC_LCTL, /**/ KC_LCTL, KC_LGUI, KC_LALT, NUMPAD,  LOWER,   KC_SPC, /**/ KC_ENT,  RAISE,   RAISE,   KC_RALT, KC_RGUI, KC_RCTL, /**/ KC_0
+  _______, /**/ KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   /**/ KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, /**/ _______,
+  _______, /**/ KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,   /**/ KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,  /**/ _______,
+  _______, /**/ KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,   /**/ KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, /**/ _______,
+  _______, /**/ KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   /**/ KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, /**/ _______,
+  _______, /**/ KC_LCTL, KC_LGUI, KC_LALT, NUMPAD,  LOWER,   KC_SPC, /**/ KC_ENT,  RAISE,   RAISE,   KC_RALT, KC_RGUI, KC_RCTL, /**/ _______
 ),
 
 /* Lower
@@ -76,9 +76,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_5x14(
-  _______, /**/ KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   /**/ KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC, /**/ KC_BSPC,
-  _______, /**/ KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, /**/ KC_EQL,  KC_LBRC, KC_RBRC, KC_LPRN, KC_F11,  KC_DEL,  /**/ KC_DEL,
-  _______, /**/ KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_PSCR, KC_F5,   /**/ KC_MINS, KC_LPRN, KC_RPRN, KC_LCBR, KC_PIPE, KC_BSLS, /**/ KC_BSLS,
+  _______, /**/ KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   /**/ KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC, /**/ _______,
+  _______, /**/ KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, /**/ KC_EQL,  KC_LBRC, KC_RBRC, KC_LPRN, KC_F11,  KC_DEL,  /**/ _______,
+  _______, /**/ KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_PSCR, KC_F5,   /**/ KC_MINS, KC_LPRN, KC_RPRN, KC_LCBR, KC_PIPE, KC_BSLS, /**/ _______,
   _______, /**/ _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  /**/ KC_EQL,  KC_LCBR, KC_RCBR, KC_HOME, _______, _______, /**/ _______,
   _______, /**/ _______, _______, _______, XXXXXXX, _______, _______, /**/ _______, _______, XXXXXXX, _______, _______, _______, /**/ _______
 ),
