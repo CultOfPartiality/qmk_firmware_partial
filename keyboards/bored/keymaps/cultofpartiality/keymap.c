e072b09d54d9092ce9c3baed991a833dba238d9a
Partial <jason@wridgway.net>
2022-12-10 18:54:54 +1100

    Added grave to Bored layout

--- keyboards/bored/keymaps/cultofpartiality/keymap.c
+++ keyboards/bored/keymaps/cultofpartiality/keymap.c
@@ -63,9 +63,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_ortho_5x14(
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   /**/ _______, _______, /**/ KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC,
-  KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, /**/ _______, _______, /**/ KC_EQL,  KC_LBRC, KC_RBRC, KC_LPRN, KC_F11,  KC_DEL,
-  KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_PSCR, KC_PSCR, /**/ _______, _______, /**/ KC_MINS, KC_LPRN, KC_RPRN, KC_LCBR, KC_PIPE, KC_BSLS,
-  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  /**/ _______, _______, /**/ KC_EQL,  KC_LCBR, KC_RCBR, KC_HOME, _______, _______,
+  KC_TILD, KC_GRV,  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ _______, _______, /**/ KC_EQL,  KC_LBRC, KC_RBRC, KC_LPRN, KC_F11,  KC_DEL,
+  KC_DEL,  KC_F1,   XXXXXXX, XXXXXXX, XXXXXXX, KC_PSCR, /**/ _______, _______, /**/ KC_MINS, KC_LPRN, KC_RPRN, KC_LCBR, KC_PIPE, KC_BSLS,
+  _______, KC_F7,   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, /**/ _______, _______, /**/ KC_EQL,  KC_LCBR, KC_RCBR, KC_HOME, _______, _______,
   _______, _______, _______, XXXXXXX, _______, _______, /**/ _______, _______, /**/ _______, _______, XXXXXXX, _______, _______, _______
   ),
 
